3b9bd349e3b8c2397a39ca8a9b8f49c79202fe85
ekzGuille <guillermosesesantos@hotmail.com>
2018-12-29 12:43:32 +0100

    show year percent

---
+++ day-today/DateCounter.js
@@ -0,0 +1,22 @@
+class DateCounter {
+  constructor() {}
+
+  mapping(variable, i_min, i_max, o_min, o_max) {
+    return (variable - i_min) * (o_max - o_min) / (i_max - i_min) + o_min;
+  }
+
+  getPercent() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const start = new Date(date.getFullYear(), 0, 0);
+    const diff = (date - start) + ((start.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000);
+    const oneDay = 1000 * 60 * 60 * 24
+    const currentDay = Math.floor(diff / oneDay);
+    const minDay = 0;
+    const maxDay = (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) ? 366 : 365;
+    const minPer = 0;
+    const maxPer = 100;
+    const per = this.mapping(currentDay, minDay, maxDay, minPer, maxPer);
+    return ~~(per*100)/100;
+  }
+}
--- day-today/sketch.js
+++ day-today/sketch.js
@@ -1,7 +1,14 @@
+let date = new DateCounter();
+let size = 200;
+let offset = size + size/2
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(0);
+  fill(255);
+  textSize(size);
+  text(`${date.getPercent()}%`, (windowWidth / 2) - offset, (windowHeight / 2));
 }
